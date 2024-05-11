afeae1eea421cd1979407d3242c2d4632e8198a2
Ernesto Crespo <ecrespo@protonmail.com>
2024-05-11 12:46:06 -0400

    Added CICD

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,10 @@ import reflex as rx
 
 config = rx.Config(
     app_name="webpage",
-    api_url="https://webpage-production-33d3.up.railway.app/",
+    api_url="https://webpage-production-9957.up.railway.app/",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://api.seraph13.dev",
+        "https://webpage-production-9957.up.railway.app/"
+    ],
 )
